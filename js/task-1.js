0f23c92e5b911b02d563714400a5d44686f54383
Oksana Ilnytska <okailnytska@gmail.com>
2024-07-18 00:55:44 +0200

    add files and tasks one and two

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+   
+    if (customerCredits >= quantity * pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${quantity* pricePerDroid} credits!`;
+   } else {
+    return `Insufficient funds!`;
+   }
+}
+
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
+
